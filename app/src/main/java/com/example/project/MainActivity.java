253d254aa480f54f42fce6d5e01c3c8341fabf6d
b22thohe <tomhell66@gmail.com>
2024-04-28 22:05:51 +0200

    Add read from shared preferences to String Add getters/setters for member variable myPreferenceData to hold shared preference value

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.project;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     // Member variables
     private SharedPreferences myPreferenceRef;
     private SharedPreferences.Editor myPreferenceEditor;
+
+    private String myPreferenceData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,23 @@ public class MainActivity extends AppCompatActivity {
         myPreferenceRef = getPreferences(MODE_PRIVATE);
         // Create SharedPreference Editor
         myPreferenceEditor = myPreferenceRef.edit();
+
+        // Read data from SharedPreference
+        //TextView prefTextRef = new TextView(this);
+        //prefTextRef = (TextView)findViewById(R.id.myTextView);
+        //prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference string found!"));
+        String str = myPreferenceRef.getString("MyAppPreferenceString", "No preference string found!");
+        setMyPreferenceData(str);
     }
 
+    // Getter for myPreferenceData
+    public String getMyPreferenceData() {
+        return myPreferenceData;
+    }
+
+    // Setter for myPreferenceData
+    public void setMyPreferenceData(String str) {
+        myPreferenceData = str;
+        Log.d("--> myPreferenceData: ", str);
+    }
 }
