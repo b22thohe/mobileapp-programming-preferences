d68a06df3b910bc81f5df51f0fe8ebe4cc95274c
b22thohe <tomhell66@gmail.com>
2024-04-28 21:23:12 +0200

    Create SharedPreference object and editor

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    // Member variables
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Create SharedPreference object
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        // Create SharedPreference Editor
+        myPreferenceEditor = myPreferenceRef.edit();
     }
 
 }
