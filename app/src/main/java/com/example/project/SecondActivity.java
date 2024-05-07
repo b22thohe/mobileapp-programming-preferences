9021c5609abd3e650d7c0f084402bb8bbe629ad2
b22thohe <tomhell66@gmail.com>
2024-05-07 14:55:48 +0200

    Add Button and listener to button

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 //import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -25,6 +28,19 @@ public class SecondActivity extends AppCompatActivity {
                 Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
                 v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
                 return insets;
+            }
+        });
+
+        // Add button object
+        Button saveButton = findViewById(R.id.saveButton);
+        // Add click listener to button
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d("-->", "myButton in MainActivity was pressed!");
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(intent);
             }
         });
     }
