2d9c11787acd0dcee4fadd84bca804bacf6d2e39
b22thohe <tomhell66@gmail.com>
2024-05-07 17:25:26 +0200

    Create SharedPreferences object Add call to savePref() method in button listener Add savePref() method to save to shared preferences and end activity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,10 +1,12 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 //import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -15,6 +17,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,11 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
 
+        // Create SharedPreference object
+        myPreferenceRef = getSharedPreferences("mySharedPreferences", MODE_PRIVATE);
+        // Create SharedPreference Editor
+        myPreferenceEditor = myPreferenceRef.edit();
+
         // Add button object
         Button saveButton = findViewById(R.id.saveButton);
         // Add click listener to button
@@ -39,9 +49,30 @@ public class SecondActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("-->", "myButton in MainActivity was pressed!");
-                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
-                startActivity(intent);
+                savePref(v);
             }
         });
+    }
+
+    // Method to save text to shared preferences
+    public void savePref(View v) {
+        // Get the text from EditText
+        EditText newPrefText = (EditText)findViewById(R.id.settingsText);
+
+        // Store the new preference text
+        myPreferenceEditor.putString("MyPreferenceString", newPrefText.getText().toString());
+        myPreferenceEditor.apply();
+
+        Log.d("-->SecondActivity", "Saved value: " + newPrefText.getText().toString());
+
+        // Start the MainActivity again
+        Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+        startActivity(intent);
+
+        // Clear the EditText
+        newPrefText.setText("");
+
+        // Finish the SecondActivity
+        finish();
     }
 }
